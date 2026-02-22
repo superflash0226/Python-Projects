9205afb2763abcad648046e050367f42a788d136
superflash0226 <dashangbaraiya0213@gmail.com>
2026-02-22 14:44:35 +0530

    Initial commit - website link parser
    This script parses website links from a given text file.

---
+++ website-link-parser.py
@@ -0,0 +1,43 @@
+import datetime
+now = datetime.datetime.now()
+timestamp_str = now.strftime("\n %d/%m/%Y \n %H:%M:%S\n")
+print(f" TimeStamp {timestamp_str}")
+
+import requests
+import re
+
+url = "https://arcraiders.com/"
+
+headers = {
+    "User-Agent": "Google/5.0"
+}
+
+response = requests.get(url, headers=headers)
+
+with open("game.html", "w", encoding="utf-8") as f:
+    f.write(response.text)
+
+print("HTML page saved successfully as contact_us.html")
+
+with open("game.html", "r", encoding="utf-8") as f:
+    content = f.read()
+
+link_pattern = r"https?://[^\s\"'>]+"
+
+links = re.findall(link_pattern, content)
+
+link_unique = set(links)
+
+with open("game_output.txt", "w", encoding="utf-8") as f:
+
+    f.write("Link Pattern Used:\n")
+    f.write(link_pattern + "\n\n")
+
+    if link_unique:
+        f.write("Links Found:\n")
+        for link in link_unique:
+            f.write(link + "\n")
+    else:
+        f.write("No links found on this webpage.\n")
+
+print("Links saved successfully in contact_us_output.txt")
